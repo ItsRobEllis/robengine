af94add6d0309e16a5fc921341b19efaa4dafd8a
ItsRobEllis <itsrobellis@live.co.uk>
2017-12-08 16:54:26 +0000

    Initial commit to new repo

---
+++ include/Stdafx.h
@@ -0,0 +1,26 @@
+// Game Engine Programming Assignment
+// By Rob Ellis
+// Bournemouth University
+
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+// Disable warning for localtime
+#pragma warning(disable : 4996)
+
+// System Includes
+#include <conio.h>
+#include <iomanip>
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <Windows.h>
+
+// SDL & glew
+#include "SDL.h"
+#include "glew.h"
+
+// Class Headers
+#include "Core.h"
+
+#endif
---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+// Game Engine Programming Assignment
+// By Rob Ellis
+// Bournemouth University
+
+#include "Stdafx.h"
+
+int main(int argc, char* args[])
+{
+	Core *m_testCore = new Core();
+	return m_testCore->Launch();
+}
